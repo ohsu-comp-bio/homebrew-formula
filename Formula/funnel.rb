bc6f670776ee997e78f5ef95a17325a32251246f
Alex Buchanan <buchanae@ohsu.edu>
2018-01-16 11:39:19 -0800

    funnel version 0.5.0

--- Formula/funnel.rb
+++ Formula/funnel.rb
@@ -1,8 +1,8 @@
 class Funnel < Formula
   desc "distributed task execution toolkit"
   homepage "https://ohsu-comp-bio.github.io/funnel/"
-  url "https://github.com/ohsu-comp-bio/funnel/releases/download/0.4.1/funnel-darwin-amd64-0.4.1.tar.gz"
-  sha256 "bd00ad8b973c7ee545933fd14287e8c0cc427822a4ac4984ebf47f449a99eb16"
+  url "https://github.com/ohsu-comp-bio/funnel/releases/download/0.5.0/funnel-darwin-amd64-0.5.0.tar.gz"
+  sha256 "c746f895664cee9051d457797a67fc92418c4b454ed3434daccd07854a358d49"
 
   bottle :unneeded
 
